096b5dbd10205254bae360add4a56987d4cf0513
Cris GC <cris.gcc@outlook.com>
2026-09-11 23:59:03 -0600

    Login solo diseño

---
+++ src/domains/auth/components/LoginForm.tsx
@@ -0,0 +1,59 @@
+import { CardBasico } from "@/components/ui/CardBasico";
+import { InputLabelIcon } from "@/components/ui/InputLabelIcon";
+import { ButtonIcon } from "@/components/ui/ButtonIcon";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiLockLine } from "react-icons/ri";
+import { SlLogin } from "react-icons/sl";
+
+export function LoginForm() {
+    return (
+        <CardBasico>
+            <div className="flex flex-col px-2 py-1">
+                <div className="mt-1.5 mb-6">
+                    <h5 className="h5 text-text-primay">
+                        ¡Bienvenido de nuevo!
+                    </h5>
+                    <p className="text-sm text-text-secondary">
+                        Ingrese sus credenciales para iniciar sesión
+                    </p>
+                </div>
+                <div className="mb-3">
+                    <InputLabelIcon
+                        type="text"
+                        label="Email"
+                        icon={<MdOutlineEmail />}
+                        placeholder="Email"
+                        className="w-full text-sm"
+                    />
+                </div>
+                <div className="mb-3">
+                    <InputLabelIcon
+                        type="password"
+                        label="Contraseña"
+                        icon={<RiLockLine />}
+                        placeholder="Password"
+                        className="w-full text-sm"
+                    />
+                </div>
+                <div className="flex justify-center mb-4">
+                    <a href="#" className="text-xs text-text-secondary">
+                        ¿Olvidaste tu contraseña?
+                    </a>
+                </div>
+                <div className="mb-4">
+                    <ButtonIcon
+                        icon={<SlLogin />}
+                        label="Iniciar Sesión"
+                        className="w-full text-sm text-white bg-primary hover:bg-primary-hover"
+                        onClick={() => alert("Iniciando sesión...")}
+                    />
+                </div>
+                <div className="mb-4 text-center">
+                    <p className="text-xs text-text-secondary">
+                        Al continuar, aceptas los <a href="#" className="text-text-primary underline">Términos de Servicio</a> y la <a href="#" className="text-text-primary underline">Política de Privacidad</a>
+                    </p>
+                </div>
+            </div>
+        </CardBasico>
+    )
+}
--- src/domains/auth/pages/LoginPage.tsx
+++ src/domains/auth/pages/LoginPage.tsx
@@ -1,7 +1,20 @@
+import LoginBanner from '@/assets/images/banners/login.png';
+import { LoginForm } from '@/domains/auth/components/LoginForm';
+import { BasicInfoLogin } from '@/domains/auth/components/BasicInfoLogin';
 export function LoginPage() {
     return (
-        <div>
-            <h1>Login</h1>
+        <div
+            className="min-h-screen w-full flex bg-cover bg-center bg-no-repeat p-4 relative"
+            style={{ backgroundImage: `url(${LoginBanner})` }}
+        >
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full">
+                <div className="flex justify-center py-10">
+                    <BasicInfoLogin />
+                </div>
+                <div className="flex justify-center items-center">
+                    <LoginForm />
+                </div>
+            </div>
         </div>
     )
 }
