06d4a56719403be437bf57098a96333bab91ceec
Cris GC <cris.gcc@outlook.com>
2026-09-12 01:15:00 -0600

    Ajustes mínimos

--- src/components/ui/ButtonIcon.tsx
+++ src/components/ui/ButtonIcon.tsx
@@ -1,5 +1,5 @@
 interface ButtonIconProps {
-    icon: React.ReactNode;
+    icon?: React.ReactNode;
     label?: string;
     className?: string;
     onClick?: () => void;
@@ -10,12 +10,16 @@ export function ButtonIcon({
     className,
     onClick
 }: ButtonIconProps) {
+    const separatore = () => {
+        if (label && icon) return "pl-2"
+        else return ''
+    }
     return (
         <button
             onClick={onClick}
             className={`cursor-pointer flex items-center justify-center px-2 py-1 rounded-md transition-colors ${className}`}
         >
-            {label}<span className="pl-2">{icon}</span>
+            {label}<span className={separatore()}>{icon}</span>
         </button>
     )
 }
