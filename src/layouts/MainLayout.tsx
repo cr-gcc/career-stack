41fd2ae5e4d512f69e97718131ea9502fd9e1ff9
Cris GC <cris.gcc@outlook.com>
2026-09-12 01:15:20 -0600

    Reacomode de componentes estructurales

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,17 @@
 import { Outlet } from 'react-router'
 import { Header } from "@components/Header"
-import { Footer } from "@components/Footer"
+import { SideBar } from "@components/SideBar"
 
 export function MainLayout() {
     return (
-        <div className="min-h-screen flex flex-col">
-            <Header />
-            <main id="main" className="bg-main flex-1 p-4 mb-18">
-                <div className="mx-auto w-full px-4 sm:px-6 lg:px-8">
+        <div className="flex min-h-screen bg-background">
+            <SideBar />
+            <div className="flex flex-1 flex-col overflow-hidden">
+                <Header />
+                <main id="main" className="flex-1 overflow-y-auto p-4 sm:px-6 lg:px-8">
                     <Outlet />
-                </div>
-            </main>
-            <Footer />
+                </main>
+            </div>
         </div>
     )
 }
