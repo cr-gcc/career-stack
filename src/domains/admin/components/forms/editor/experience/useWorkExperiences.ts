a7322e754e6f892ea7ec68e1f8e477d316a229f6
Cris GC <cris.gcc@outlook.com>
2026-09-17 00:13:29 -0600

    Mejora del hook de experiencia

--- src/domains/admin/components/forms/editor/experience/useWorkExperiences.ts
+++ src/domains/admin/components/forms/editor/experience/useWorkExperiences.ts
@@ -3,25 +3,30 @@ import type { CvWorkExperience } from "@/types/cv.types";
 
 export function useWorkExperiences() {
     const { cv, updateField } = useCvStore();
-    const workExperiences = cv.workExperiences;
 
     const addExperience = (type: 'full-time' | 'part-time') => {
-        updateField('workExperiences', [
-            ...workExperiences,
-            { id: Date.now(), name: '', dateRange: '', role: '', description: '', type }
-        ]);
+        const workExperiences = [...cv.workExperiences];
+        workExperiences.push({ id: Date.now(), name: '', dateRange: '', role: '', description: '', type });
+        updateField('workExperiences', workExperiences);
     };
 
     const removeExperience = (type: 'full-time' | 'part-time') => {
+        const workExperiences = [...cv.workExperiences];
         const experiencesOfType = workExperiences.filter(exp => exp.type === type);
         if (experiencesOfType.length === 0) return;
-        const lastExperience = experiencesOfType[experiencesOfType.length - 1];
-        updateField('workExperiences', workExperiences.filter(exp => exp.id !== lastExperience.id));
+        workExperiences.pop();
+        updateField('workExperiences', workExperiences);
     };
 
     const updateExperience = (id: number, key: keyof CvWorkExperience, value: string) => {
-        updateField('workExperiences', workExperiences.map((exp) => (exp.id === id ? { ...exp, [key]: value } : exp)));
+        const updatedExperiences = cv.workExperiences.map((exp) => {
+            if (exp.id === id) {
+                return { ...exp, [key]: value }
+            }
+            return exp
+        });
+        updateField('workExperiences', updatedExperiences);
     };
 
-    return { workExperiences, addExperience, removeExperience, updateExperience };
+    return { workExperiences: cv.workExperiences, addExperience, removeExperience, updateExperience };
 }
